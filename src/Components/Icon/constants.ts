9faa50086c2443f4f808043ead15b8bc71a44f4d
AndriiB <bezprizvanyja@gmail.com>
2023-04-02 18:43:35 +0300

    Setting prettier, removing unnecessary

--- src/Components/Icon/constants.ts
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as BrainIcon } from "./icons/brain.svg";
-import { ReactComponent as PlusIcon } from "./icons/plus.svg";
-import { ReactComponent as ReadIcon } from "./icons/read.svg";
-
-export const ICONS = {
-  brain: BrainIcon,
-  plus: PlusIcon,
-  read: ReadIcon,
-};
