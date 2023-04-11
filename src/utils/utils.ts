4ca778dd37f4cfa6951497ce423858559dc73aa2
Andrii Bezpyzvanyi <bezprizvanyja@gmail.com>
2023-04-11 17:33:54 +0300

    Trimming spaces at the beginning and end of a line

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
 export const normalizeCase = (word: string) => word[0].toUpperCase() + word.slice(1).toLowerCase();
 
 export const normalizeCaseForWord = (word: Word) => ({
-  word: normalizeCase(word.word),
-  translate: normalizeCase(word.translate),
+  word: normalizeCase(word.word.trim()),
+  translate: normalizeCase(word.translate.trim()),
   mark: word.mark,
 });
 
@@ -27,7 +27,7 @@ export const getWordsFromLocaleStorage = () => {
 export const addWordToLocalStorage = (word: Word) =>
   localStorage.setItem(
     "w",
-    `${normalizeCase(word.word)}*${normalizeCase(word.translate)}*${word.mark}.` + getStringWordsFromLocaleStorage(),
+    `${normalizeCase(word.word.trim())}*${normalizeCase(word.translate.trim())}*${word.mark}.` + getStringWordsFromLocaleStorage(),
   );
 
 export const updateWordInLocaleStorage = (word: Word) => {
